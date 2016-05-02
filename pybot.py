d5a6dd73d51595dc715bfaf6b447c29f24491096
alexpkeil1 <alex.keil@gmail.com>
2016-05-01 23:08:44 -0400

    fixed pybot to use random quote

--- pybot.py
+++ pybot.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # send a tweet from a text file using the Tweepy module
+# finds a random Nietzche quote
 
 # -*- coding: utf-8 -*-
  
 import mybotapi as mpi
 import tweepy
-
+import requests
+from lxml import html
 
 t_keys = mpi.get_keys()
 
@@ -14,10 +16,29 @@ auth.set_access_token(t_keys['ACCESS_KEY'], t_keys['ACCESS_SECRET'])
 api = tweepy.API(auth)
 
 
-first_tweet = 'test_tweet.txt'
 
-with open(first_tweet, 'rb') as f:
-    api.update_status(f.read())
+
+
+resp = requests.get("http://www.nietzschefamilycircus.com")
+
+rt = html.fromstring(resp.text)
+divs = rt.cssselect('div')
+quote = [i.text for i in divs][7].replace('\n', '').replace('\t', '')
+
+
+first_tweet = '/home/akeil/Documents/programming_examples/python/twitterbottest_tweet.txt'
+with open(first_tweet, 'w') as f:
+    f.write(quote)
+
+
+with open(first_tweet, 'r') as f:
+    print(f.read())
+
+if quote.find('wom') < 0:
+    print('tweeting: ' + quote)
+    #api.update_status(quote)
+else: 
+    print('not tweeting')
 
 
     
