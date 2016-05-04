b0759b94ffcc3d67f9892883accedeb504f698a8
alexpkeil1 <alex.keil@gmail.com>
2016-05-04 11:00:56 -0400

    linux stuff

---
+++ mybotapi.py
@@ -0,0 +1,15 @@
+import os
+
+
+os.chdir("/home/akeil/Documents/programming_examples/python/twitterbot")
+
+
+#enter the corresponding information from your Twitter application:
+ 
+def get_keys():
+    dict = {}
+    dict['CONSUMER_KEY'] = 'Pg5QBoiOOKD0rv1wkJWSvCape'#keep the quotes, replace this with your consumer key
+    dict['CONSUMER_SECRET'] = 'PcksTh8dRgebvfScD2jjlYu1yRsjT5Yzuhqc5Ii6PIOxwy3yiA'#keep the quotes, replace this with your consumer secret key
+    dict['ACCESS_KEY'] = '726821204334628864-lvFIom98ak8BuRFM2knxHXPkefyo9fZ'#keep the quotes, replace this with your access token
+    dict['ACCESS_SECRET'] = 't4kRFCyXwtGCaIg7ySEV0zI1HSFzozd1UsOSBouE0liTz'#keep the quotes, replace this with your access token secret
+    return dict
