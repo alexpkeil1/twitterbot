9198c45d598733300133b2406221700aad91c99a
alexpkeil1 <alex.keil@gmail.com>
2016-05-04 10:58:28 -0400

    some stuff on linux

---
+++ tweet_quote.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# send a tweet from a text file using the Tweepy module
+
+# -*- coding: utf-8 -*-
+ 
+import mybotapi as mpi
+import tweepy
+import requests
+
+
+first_tweet = 'test_tweet.txt'
+
+
+def tweet(textfile):
+    auth = tweepy.OAuthHandler(t_keys['CONSUMER_KEY'], t_keys['CONSUMER_SECRET'])
+    auth.set_access_token(t_keys['ACCESS_KEY'], t_keys['ACCESS_SECRET'])
+    api = tweepy.API(auth)
+    with open(textfiletext, 'rb') as f:
+        api.update_status(f.read())
+
+
+    
+#tweet(first_tweet)
