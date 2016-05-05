4ed5a808b1673adfccd0b457a2138916a9be6c85
Alex Keil <alex.keil@gmail.com>
2016-05-05 15:41:44 -0400

    cleaning up

--- tweet_from_file_bot.py
+++ tweet_from_file_bot.py
@@ -3,24 +3,28 @@
 
 # -*- coding: utf-8 -*-
  
-import mybotapi as mpi
 import tweepy
 import requests
 import os
-os.chdir("/Users/akeil/Documents/programming_examples/python/twitterbot/")
+if os.sys.platform == 'darwin':
+    base ="/Users/akeil"
+else:
+    base = "/home/akeil"
+
+os.chdir(base + "/Documents/programming_examples/python/twitterbot/")
 import mybotapi as mpi # need to cd into this directory
 
 
-first_tweet = 'test_tweet.txt'
+first_tweet = base + "/Documents/programming_examples/python/twitterbot/" + 'test_tweet.txt'
 
 
 def tweet(textfile):
     auth = tweepy.OAuthHandler(t_keys['CONSUMER_KEY'], t_keys['CONSUMER_SECRET'])
     auth.set_access_token(t_keys['ACCESS_KEY'], t_keys['ACCESS_SECRET'])
     api = tweepy.API(auth)
-    with open(textfiletext, 'rb') as f:
-        api.update_status(f.read())
+    with open(textfile, 'rb') as f:
+        api.update_status(f.read().decode('utf-8'))
 
 
     
-#tweet(first_tweet)
+tweet(first_tweet)
