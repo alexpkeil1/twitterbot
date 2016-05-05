0776eba20ed11eece16b18c17f7c4f0194ca840d
Alex Keil <alex.keil@gmail.com>
2016-05-05 16:16:44 -0400

    attribution better

--- nietzche_bot.py
+++ nietzche_bot.py
@@ -43,7 +43,7 @@ with open(first_tweet, 'r') as f:
 
 if quote.find('wom') < 0:
     print('tweeting: ' + quote)
-    tweet = api.update_status(quote)
+    tweet = api.update_status(quote.strip() + ' (Nietzsche)')
 else: 
     print('not tweeting')
 
