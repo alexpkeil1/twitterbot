128d091be74fe4d3d24646f48c58a7bb44fb0a0c
Alex Keil <alex.keil@gmail.com>
2016-05-05 15:49:52 -0400

    file bot better

--- tweet_from_file_bot.py
+++ tweet_from_file_bot.py
@@ -6,6 +6,7 @@
 import tweepy
 import requests
 import os
+from numpy import random
 if os.sys.platform == 'darwin':
     base ="/Users/akeil"
 else:
@@ -16,15 +17,18 @@ import mybotapi as mpi # need to cd into this directory
 
 
 first_tweet = base + "/Documents/programming_examples/python/twitterbot/" + 'test_tweet.txt'
+joke_tweet = base + "/Documents/programming_examples/python/twitterbot/" + 'jokes.txt'
 
 
 def tweet(textfile):
     auth = tweepy.OAuthHandler(t_keys['CONSUMER_KEY'], t_keys['CONSUMER_SECRET'])
     auth.set_access_token(t_keys['ACCESS_KEY'], t_keys['ACCESS_SECRET'])
     api = tweepy.API(auth)
+    lines = []
     with open(textfile, 'rb') as f:
-        api.update_status(f.read().decode('utf-8'))
+        for l in f:
+            lines.append(l.decode('utf-8').strip())
+    random.shuffle(lines)
+    api.update_status(lines.pop())
 
-
-    
-tweet(first_tweet)
+tweet(joke_tweet)
